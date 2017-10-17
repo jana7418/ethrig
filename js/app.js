fcbdf8717ddd30872e2eb79c8b5ca1f5d6029e38
jana7418 <jana7418@gmail.com>
2017-10-18 02:52:55 +0530

    Added Payment Details

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 var address = "0x56db9d7156ec324f2d4f998916497c756fd87fbf";
 var apiUrl = "https://api.nanopool.org/v1/eth"; // No Trailing / to it
-var apiRequest = "balance_hashrate";
 //API mainUrl
-var mainURL = apiUrl + '/' + apiRequest + '/' + address;
+var mainURL = apiUrl + '/' + "balance_hashrate" + '/' + address;
+var paymentUrl = apiUrl + '/' + "payments" + '/' + address;
 
 fetch(mainURL, {method: 'get'})
 .then((res) => res.json())
@@ -17,14 +17,26 @@ if (hashrate != 0){
   document.getElementById("status1").style.display = "none";
   document.getElementById("status2").style.display = "";
 }
-document.getElementById('balance').innerHTML = balance +' ETH';
-document.getElementById('progressbar').innerHTML = `
-    <div class="progress-bar progress-bar-striped bg-success"
-    role="progressbar"
-    style="width: ${percent}%"
-    aria-valuenow="25"
-    aria-valuemin="0"
-    aria-valuemax="100">
-    </div>;
-`
+  document.getElementById('balance').innerHTML = balance +' ETH';
+  document.getElementById('progressbar').innerHTML = `
+      <div class="progress-bar progress-bar-striped bg-success"
+      role="progressbar"
+      style="width: ${percent}%"
+      aria-valuenow="25"
+      aria-valuemin="0"
+      aria-valuemax="100">
+      </div>
+  `;
+
+});
+
+fetch(paymentUrl, {method: 'get'})
+.then((res) => res.json())
+.then((response) =>{
+  var data = response.data;
+  document.getElementById('list1').innerHTML = data.length;
+  document.getElementById('list2').innerHTML = data[0].date;
+  document.getElementById('list3').innerHTML = data[0].txHash;
+  document.getElementById('list4').innerHTML = data[0].amount;
+
 });
